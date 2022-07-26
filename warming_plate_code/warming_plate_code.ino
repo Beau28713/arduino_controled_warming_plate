a857fc6fd6884ec7629c258b1faa5854f60fe02c
Beau Clark <beau28713@gmail.com>
2022-07-26 07:05:24 -0400

    Add files via upload

--- warming_plate_code/warming_plate_code.ino
+++ warming_plate_code/warming_plate_code.ino
@@ -2,19 +2,23 @@
 #include <LiquidCrystal.h>
 
 Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(10, 11, 12, 13);
+
 const int rs = 9, en = 8, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+int relay_pin = 7;
+int increment_button = 6;
+int decrement_button = 0;
+int change_temp_button = 1;
+
 void setup() 
 {
+  pinMode(relay_pin, OUTPUT);
+  pinMode(increment_button, INPUT);
+  pinMode(decrement_button, INPUT);
+  pinMode(change_temp_button, INPUT);
+  
   lcd.begin(20, 4);
-  /*Serial.begin(115200);
-  while (!Serial) 
-  {
-    delay(10);
-  }
-  */
-  lcd.print("Hello Beau");
 
   maxthermo.begin();
 
@@ -23,16 +27,37 @@ void setup()
 
 void loop() 
 {
-  read_and_display_temp();
+  float surface_temp = read_and_display_temp();
+  
+  int button = digitalRead(change_temp_button);
+  
+  if(button == HIGH)
+  {
+    change_temp();
+  }
+
+  if(surface_temp < 100)
+  {
+    digitalWrite(relay_pin, HIGH);
+  }
+  else
+  {
+    digitalWrite(relay_pin, LOW);
+  }
 
   delay(1000);
 }
 
-void read_and_display_temp()
+float read_and_display_temp()
 {
   float thermo_temp = (maxthermo.readThermocoupleTemperature() * 1.8) + 32.0;
-  //Serial.println(thermo_temp);
-  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(thermo_temp);
+  return thermo_temp;
+}
+
+void change_temp()
+{
+  lcd.setCursor(0, 1);
+  lcd.print("Inside function");
 }
