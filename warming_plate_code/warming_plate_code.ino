634dfc5fb39da8a36714108a9831314f4f02d2d8
Beau Clark <beau28713@gmail.com>
2022-07-27 06:51:17 -0400

    Added basic temp control

--- warming_plate_code/warming_plate_code.ino
+++ warming_plate_code/warming_plate_code.ino
@@ -8,8 +8,9 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 int relay_pin = 7;
 int increment_button = 6;
-int decrement_button = 0;
+int decrement_button = A1;
 int change_temp_button = 1;
+int temp_setpoint = 70;
 
 void setup() 
 {
@@ -29,14 +30,15 @@ void loop()
 {
   float surface_temp = read_and_display_temp();
   
-  int button = digitalRead(change_temp_button);
+  int temp_button = digitalRead(change_temp_button);
+  delay(200);
   
-  if(button == HIGH)
+  if(temp_button == HIGH)
   {
     change_temp();
   }
 
-  if(surface_temp < 100)
+  if(surface_temp < temp_setpoint)
   {
     digitalWrite(relay_pin, HIGH);
   }
@@ -45,7 +47,7 @@ void loop()
     digitalWrite(relay_pin, LOW);
   }
 
-  delay(1000);
+  delay(2000);
 }
 
 float read_and_display_temp()
@@ -53,11 +55,39 @@ float read_and_display_temp()
   float thermo_temp = (maxthermo.readThermocoupleTemperature() * 1.8) + 32.0;
   lcd.setCursor(0, 0);
   lcd.print(thermo_temp);
+  lcd.setCursor(0, 1);
+  lcd.print(temp_setpoint);
   return thermo_temp;
 }
 
 void change_temp()
 {
-  lcd.setCursor(0, 1);
-  lcd.print("Inside function");
+  lcd.clear();
+  lcd.print("Chnage Temp");
+  
+  do
+  {
+    lcd.setCursor(0, 1);
+    lcd.print(temp_setpoint);
+    
+    int temp_up = digitalRead(increment_button);
+    delay(200);
+    int temp_down = digitalRead(decrement_button);
+    delay(200);
+
+    if(temp_up == HIGH)
+    {
+      temp_setpoint += 1;
+    }
+    else if(temp_down == HIGH)
+    {
+      temp_setpoint -= 1;
+    }
+  }
+  while(digitalRead(change_temp_button) == HIGH);
+
+  lcd.clear();
+  lcd.print("Exited Function");
+  delay(1000);
+  lcd.clear();
 }
